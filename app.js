1606ace5e0d91068bc41f0f2850d8de0c1aa7b9b
TekniqueJoseph <tekniquejoseph@gmail.com>
2023-08-06 13:40:24 -0400

    code for study of async / await

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const getPosition = (opts) => {
   return promise;
 };
 
-const setTimer = (duration) => {
+const setTimer = duration => {
   const promise = new Promise((resolve, reject) => { // constructor function / Class built into JS - function passed into the constructor is exe right away. Takes to arguments
     setTimeout(() => { //timer is set when promise is created. Async code exe after timer is done
       resolve('Done'); // JS engine internally marks the promise as resolved/done. called when timer is done
@@ -22,24 +22,26 @@ const setTimer = (duration) => {
   return promise; // returned after creating the promise. can be used where you call setTimer
 }
 
-function trackUserHandler() { //callBack
-  let positionData; // initiallizes variable for position data
-  getPosition()
-    .then(posData => { //posData received from getPosition resolve
-      positionData = posData; //receives posData from getPosition
-      return setTimer(2000) // calls setTimer and passes duration. back to pending status waiting for seTtimer
-    })
-    .catch( err => { // will cancel any prior thens if one fails any after will still execute
-      console.log(err)
-      return 'on we go...'
-    })
-    .then(data => { // promise chaining. pending and executed after getPosition is resolved
-      console.log(data, positionData) //recieves data from setTimer and positon data from get position after its call to setTimer is resolved
-    });
-  setTimer(1000).then(() =>{ // returns a Promise. then executes when the promise is resolved. Passes duration to setTimer.
-    console.log('Timer Done')
-      })
-    console.log('Getting Position...') // runs first
+async function trackUserHandler() { //callBack. with async the function wraps code into a promise and automatically returns a promise. internally then mechanisms are still performed
+  // let positionData; // initiallizes variable for position data
+  const posData = await getPosition() // returns a promise. await waits  for the promise to resolve or fail 
+  const timerData = await setTimer(2000)
+  console.log(timerData, posData)
+    // .then(posData => { //posData received from getPosition resolve
+    //   positionData = posData; //receives posData from getPosition
+    //   return setTimer(2000) // calls setTimer and passes duration. back to pending status waiting for seTtimer
+    // })
+    // .catch( err => { // will cancel any prior thens if one fails any after will still execute
+    //   console.log(err)
+    //   return 'on we go...'
+    // })
+    // .then(data => { // promise chaining. pending and executed after getPosition is resolved
+    //   console.log(data, positionData) //recieves data from setTimer and positon data from get position after its call to setTimer is resolved
+    // });
+  // setTimer(1000).then(() =>{ // returns a Promise. then executes when the promise is resolved. Passes duration to setTimer.
+  //   console.log('Timer Done')
+  //     })
+  //   console.log('Getting Position...') // runs first
 }
 
 button.addEventListener('click', trackUserHandler);
